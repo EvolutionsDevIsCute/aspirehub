0dbdfa509b45cecd66007f6cf17fb0399e47b1c2
EvolutionsDevIsCute <katelynnridesme@gmail.com>
2025-12-14 02:57:39 -0500

    Update button click message in test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,13 @@
+-- Lua tab example
 CreateAddonTab("Test Addon")
 AddLabel("Test Addon", "Hello from Lua!", 20, 20)
 AddButton("Test Addon", "Click Me", 20, 60, 120, 40, function()
-    print("Lua button clicked!")
+    print("Button clicked!")
+end)
+
+-- Another tab
+CreateAddonTab("Settings")
+AddLabel("Settings", "This is the settings tab", 20, 20)
+AddButton("Settings", "Toggle Console", 20, 60, 150, 40, function()
+    print("Toggle console pressed")
 end)
