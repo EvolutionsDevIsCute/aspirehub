fc89a8c7237295a65ae5f7518bbeb8a053c653da
EvolutionsDevIsCute <katelynnridesme@gmail.com>
2025-12-14 02:13:13 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,10 @@
-createWindow("Hello from Lua!")
+-- Each Lua script can fully customize its own window
+CreateWindow("Addon Window 1", 600, 400)
+AddLabel("Welcome to Addon 1", 20, 20)
+AddButton("Press Here", 20, 60, 120, 40, function()
+    print("Addon button clicked!")
+end)
+
+-- Another independent window
+CreateWindow("Addon Window 2", 400, 300)
+AddLabel("Second Window", 10, 10)
