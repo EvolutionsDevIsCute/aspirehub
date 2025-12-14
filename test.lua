02e80c8976d2d5dc060c1ba846f54d43aafac5e6
EvolutionsDevIsCute <katelynnridesme@gmail.com>
2025-12-14 02:52:58 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,13 @@
--- Create normal addon tab
+-- Normal addon tab
 CreateAddonTab("Test Addon")
 AddLabel("Test Addon", "Welcome to Test Addon!", 20, 20)
 AddButton("Test Addon", "Click Me", 20, 60, 120, 40, function()
-    print("Button clicked in Test Addon")
+    print("Button clicked!")
 end)
 
--- Create settings tab
+-- Settings tab
 CreateAddonTab("Settings")
-AddLabel("Settings", "This is settings", 20, 20)
+AddLabel("Settings", "This is settings tab", 20, 20)
 AddButton("Settings", "Toggle Console", 20, 60, 150, 40, function()
-    print("Console toggle button pressed")
+    print("Toggle console pressed")
 end)
