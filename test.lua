435abd1c16cb54ae4b023391009b80732798e6b7
EvolutionsDevIsCute <katelynnridesme@gmail.com>
2025-12-14 02:54:27 -0500

    Update labels and button messages in test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,5 @@
--- Normal addon tab
 CreateAddonTab("Test Addon")
-AddLabel("Test Addon", "Welcome to Test Addon!", 20, 20)
+AddLabel("Test Addon", "Hello from Lua!", 20, 20)
 AddButton("Test Addon", "Click Me", 20, 60, 120, 40, function()
-    print("Button clicked!")
-end)
-
--- Settings tab
-CreateAddonTab("Settings")
-AddLabel("Settings", "This is settings tab", 20, 20)
-AddButton("Settings", "Toggle Console", 20, 60, 150, 40, function()
-    print("Toggle console pressed")
+    print("Lua button clicked!")
 end)
